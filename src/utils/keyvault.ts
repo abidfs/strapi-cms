c050999559a7fb36015cee0d510a859e30e86a6a
abidfs <abidfs@gmail.com>
2023-08-09 23:09:39 +0100

    chore: added logs

--- src/utils/keyvault.ts
+++ src/utils/keyvault.ts
@@ -6,17 +6,18 @@ const loadSecrets = async () => {
   try {
     const keyvaultUrl = `https://${process.env.VAULT_NAME}.vault.azure.net`;
     console.log('keyvault url', keyvaultUrl);
-    if (keyvaultUrl) {
-      const managedIdentityClientId = process.env.MANAGED_IDENTITY_CLIENT_ID;
-      const credential = new DefaultAzureCredential({ managedIdentityClientId });
-      const client = new SecretClient(keyvaultUrl, credential);
-      
-      for await (const secret of client.listPropertiesOfSecrets()) {
-        if (secret.enabled) {
-          const key = secret.name;
-          const { value } = await client.getSecret(key);
-          secrets[key] = value;
-        }
+    const managedIdentityClientId = process.env.MANAGED_IDENTITY_CLIENT_ID;
+    console.log("managedIdentityClientId", managedIdentityClientId);
+    const credential = new DefaultAzureCredential({ managedIdentityClientId });
+    console.log("after credential");
+    const client = new SecretClient(keyvaultUrl, credential);
+    console.log("after client");
+
+    for await (const secret of client.listPropertiesOfSecrets()) {
+      if (secret.enabled) {
+        const key = secret.name;
+        const { value } = await client.getSecret(key);
+        secrets[key] = value;
       }
     }
   } catch (err) {
